ce32e96d82123cace85f313ac1140c6b03e580f5
Dev625-hub <devgaming450@gmail.com>
2025-11-01 23:34:54 +0530

    Add files via upload

---
+++ Assingment-1.py
@@ -0,0 +1,12 @@
+# Task_1
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+print("Addition:",(a+b))
+print("Subtraction:",(a-b))
+print("Multiplication:",(a*b))
+print("Division:",(a/b))
+# Task_2
+First_Name=input("Enter your first name")
+Last_Name=input("Enter your last name")
+print("Hello",First_Name,Last_Name,"!","Let's start to learn python.")
+
